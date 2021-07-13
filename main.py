a8faae954fa76d1e29e96be5c984ea7bf177fbf3
Ravdrew <53586118+Ravdrew@users.noreply.github.com>
2021-07-13 14:37:26 -0700

    prototype w/ user scoring

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import random as r
 directionValues = [[0.9, 0.1], [0.1, 0.9]] #left[left, right], right[left, right]
 numToDirection = ["left", "right"]
 sounds = ["recording1.wav", "recording2.wav", "recording3.wav"]
+winning = True
+score = 0
 
 s = p.Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)
 s.setInputDevice(1)
@@ -17,36 +19,44 @@ s.boot()
 
 #setup
 
-selectedSound = r.randrange(0, 3, 1)
-direction = r.randrange(0, 2, 1) #not inclusive, stops at 1
-modifier = r.uniform(0, 0.1)
 
-soundLeft = p.SfPlayer(sounds[selectedSound],loop=False)
-soundRight = p.SfPlayer(sounds[selectedSound],loop=False)
+while(winning):
+    selectedSound = r.randrange(0, 3, 1)
+    direction = r.randrange(0, 2, 1) #not inclusive, stops at 1
+    modifier = r.uniform(0, 0.1)
 
-mixer = p.Mixer(outs=2,chnls=1)
-mixer.addInput(voice=0, input=soundLeft)
-mixer.addInput(voice=1, input=soundRight)
+    soundLeft = p.SfPlayer(sounds[selectedSound],loop=False)
+    soundRight = p.SfPlayer(sounds[selectedSound],loop=False)
 
-print(f"sound: {selectedSound}")
-if direction == 0:
-    print(f"direct: {direction}")
-    print(f"modif: {modifier}")
-    mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier)
-    mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier)
-if direction == 1:
-    print(direction)
-    print(modifier)
-    mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier)
-    mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier)
-mixer.out()
+    mixer = p.Mixer(outs=2,chnls=1)
+    mixer.addInput(voice=0, input=soundLeft)
+    mixer.addInput(voice=1, input=soundRight)
 
-s.start()
+    #print(f"sound: {selectedSound}")
+    if direction == 0:
+        #print(f"direct: {direction}")
+        #print(f"modif: {modifier}")
+        mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier)
+        mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier)
+    if direction == 1:
+        #print(direction)
+        #print(modifier)
+        mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier)
+        mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier)
+    mixer.out()
 
-guess = input("What direction is the sound coming from? (left/right): ")
-if guess.lower() == numToDirection[direction]:
-    print("Congratulations! You are correct!")
-else:
-    print("Try again!")
+    s.start()
 
-s.gui(locals())
+    guess = input("What direction is the sound coming from? (left/right): ")
+    if guess.lower() == numToDirection[direction]:
+        score += 1
+        print("\nCongratulations! You are correct!")
+        print(f"Your score is: {score}\n")
+        s.stop()
+        continue
+    else:
+        print("\nAh, sorry, try again!")
+        print(f"Your score was {score}! Nice job!\n")
+        exit()
+
+    s.gui(locals())
