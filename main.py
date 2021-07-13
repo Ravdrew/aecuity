cb6106d36170894687225deded0944989969469f
Ravdrew <53586118+Ravdrew@users.noreply.github.com>
2021-07-13 14:18:27 -0700

    main

---
+++ main.py
@@ -0,0 +1,45 @@
+import pyo as p
+import os
+import time
+import random as r
+
+#setup
+
+directionValues = [[0.9, 0.1], [0.1, 0.9]] #left[left, right], right[left, right]
+sounds = ["recording1.wav", "recording2.wav", "recording3.wav"]
+
+s = p.Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)
+s.setInputDevice(1)
+s.setOutputDevice(4)
+s.boot()
+
+
+#setup
+
+selectedSound = r.randrange(0, 3, 1)
+direction = r.randrange(0, 2, 1) #not inclusive, stops at 1
+modifier = r.uniform(0, 0.1)
+
+soundLeft = p.SfPlayer(sounds[selectedSound],loop=False)
+soundRight = p.SfPlayer(sounds[selectedSound],loop=False)
+
+mixer = p.Mixer(outs=2,chnls=1)
+mixer.addInput(voice=0, input=soundLeft)
+mixer.addInput(voice=1, input=soundRight)
+
+print(f"sound: {selectedSound}")
+if direction == 0:
+    print(f"direct: {direction}")
+    print(f"modif: {modifier}")
+    mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier)
+    mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier)
+if direction == 1:
+    print(direction)
+    print(modifier)
+    mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier)
+    mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier)
+mixer.out()
+
+s.start()
+
+s.gui(locals())
