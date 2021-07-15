04739d214cdd375f5fb673992eb36667ef2ee6ec
Ravdrew <53586118+Ravdrew@users.noreply.github.com>
2021-07-15 14:28:11 -0700

    Added noise but may be taken out

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import random as r
 
 # setup
 
-directionValues = ((0.9, 0.1), (0.1, 0.9)) # left[left, right], right[left, right]
+directionValues = ((0.9, 0.1), (0.1, 0.9))  # left[left, right], right[left, right]
 numToDirection = ("left", "right")
-sounds = ("recording1.wav", "recording2.wav", "recording3.wav")
+sounds = ("recording1.wav", "decafCoffeeMonoFinal.wav", "recording3.wav")
 
 # set score
-with open("presets.txt", "r") as reader:
+with open("presets.txt", "r") as reader:  # opens file reader to pull saved score
     output = reader.readline()
 
 print(f"output: {output}")
@@ -21,50 +21,46 @@ else:
 print(f"score: {score}")
 # set score
 
-s = p.Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)
-s.setInputDevice(1)
-s.setOutputDevice(4)
+s = p.Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)  # settings for pyo server
+s.setInputDevice(1)  # USER DEPENDENT
+s.setOutputDevice(4)  # USER DEPENDENT
 s.boot()
 
 
 # setup
-w = open("presets.txt", "w")
+w = open("presets.txt", "w")  # opens file writer to save score
 
 while True:
-    selectedSound = r.randrange(0, 3, 1)
-    direction = r.randrange(0, 2, 1) # not inclusive, stops at 1
-    modifier = r.uniform(0, 0.1) # 0.2??
+    selectedSound = r.randrange(0, 3, 1)  # selects sound to be played
+    direction = r.randrange(0, 2, 1)  # selects direction
+    modifier = r.uniform(0, 0.1)  # adds a little bit of variety in where the sound comes from
 
-    soundLeft = p.SfPlayer(sounds[selectedSound],loop=False)
-    soundRight = p.SfPlayer(sounds[selectedSound],loop=False)
+    soundLeft = p.SfPlayer(sounds[selectedSound], loop=False)  # sets selected sound to be played
+    soundRight = p.SfPlayer(sounds[selectedSound], loop=False)
 
-    mixer = p.Mixer(outs=2,chnls=1)
+    noise = p.BrownNoise(.03).mix(2).out()
+    mixer = p.Mixer(outs=2, chnls=1)  # sets 2 outputs
     mixer.addInput(voice=0, input=soundLeft)
     mixer.addInput(voice=1, input=soundRight)
 
-    # print(f"sound: {selectedSound}")
-    if direction == 0:
-        # print(f"direct: {direction}")
-        # print(f"modif: {modifier}")
+
+    if direction == 0:  # sets volume for each side depending on direction
         mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier)
         mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier)
     if direction == 1:
-        # print(direction)
-        # print(modifier)
         mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier)
         mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier)
     mixer.out()
-
     s.start()
 
     guess = input("What direction is the sound coming from? (left/right/save): ")
-    if guess.lower() == numToDirection[direction]:
+    if guess.lower() == numToDirection[direction]:  # checks if correct
         score += 1
         print("\nCongratulations! You are correct!")
         print(f"Your score is: {score}\n")
         s.stop()
         continue
-    elif guess.lower() == "save":
+    elif guess.lower() == "save":  # handles save option
         print("Saving your score, good work!")
         print(f"Your saved score was: {score}\n")
         print("See you!")
@@ -72,7 +68,7 @@ while True:
         w.close()
         s.stop()
         exit()
-    else:
+    else:  # handles wrong answer
         print("\nAh, sorry, try again!")
         print(f"Your score was {score}! Nice job!\n")
         exit()
