4f08dfb1ce73ac2bbd03dc0d3cd2ddd6a460a128
Ravdrew <53586118+Ravdrew@users.noreply.github.com>
2021-07-16 09:40:47 -0700

    Added difficultyVolume
    This modifier increases with the player's score and makes the audio a little quieter on the dominant side and a little louder on the other side. This makes the direction less polarized and makes the sounds seem more centered.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ f = open("presets.txt", "a")
 
 # setup
 
-directionValues = ((0.9, 0.1), (0.1, 0.9))  # left[left, right], right[left, right]
+directionValues = ((0.85, 0.15), (0.15, 0.85))  # left[left, right], right[left, right]
 numToDirection = ("left", "right")
 sounds = ("recording1.wav", "decafCoffeeMonoFinal.wav", "recording3.wav")
 
@@ -16,12 +16,11 @@ sounds = ("recording1.wav", "decafCoffeeMonoFinal.wav", "recording3.wav")
 with open("presets.txt", "r") as reader:  # opens file reader to pull saved score
     output = reader.readline()
 
-print(f"output: {output}")
 if output != "":
     score = int(output)
 else:
     score = 0
-print(f"score: {score}")
+print(f"Previous Score: {score}")
 # set score
 
 s = p.Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)  # settings for pyo server
@@ -36,7 +35,7 @@ w = open("presets.txt", "w")  # opens file writer to save score
 while True:
     selectedSound = r.randrange(0, 3, 1)  # selects sound to be played
     direction = r.randrange(0, 2, 1)  # selects direction
-    modifier = r.uniform(0, 0.1)  # adds a little bit of variety in where the sound comes from
+    modifier = r.uniform(0, 0.125)  # adds a little bit of variety in where the sound comes from
 
     soundLeft = p.SfPlayer(sounds[selectedSound], loop=False)  # sets selected sound to be played
     soundRight = p.SfPlayer(sounds[selectedSound], loop=False)
@@ -45,17 +44,23 @@ while True:
     if noiseVolume > 0.07:
         noiseVolume = 0.07
 
+    difficultyVolume = score*0.01
+    if difficultyVolume > 0.1:
+        difficultyVolume = 0.1
+
     noise = p.BrownNoise(noiseVolume).mix(2).out()
     mixer = p.Mixer(outs=2, chnls=1)  # sets 2 outputs
     mixer.addInput(voice=0, input=soundLeft)
     mixer.addInput(voice=1, input=soundRight)
 
     if direction == 0:  # sets volume for each side depending on direction
-        mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier)
-        mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier)
+        #mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] - modifier - difficultyVolume)
+        mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] + modifier + difficultyVolume)
+        print(directionValues[direction][1] + modifier + difficultyVolume)
     if direction == 1:
-        mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier)
-        mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier)
+        mixer.setAmp(vin=0,vout=0,amp=directionValues[direction][0] + modifier + difficultyVolume)
+        print(directionValues[direction][0] + modifier + difficultyVolume)
+        #mixer.setAmp(vin=1,vout=1,amp=directionValues[direction][1] - modifier - difficultyVolume)
     mixer.out()
     s.start()
 
